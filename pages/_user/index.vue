7db0c28dee3ba67d54603fbb358944da2dd0b7c1
“azoom-nguyen-ngoc-son” <nguyen.ngoc.son@azoom.com.jp>
2023-03-17 15:28:35 +0700

    :tada: base code

---
+++ pages/_user/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>
+            detail user
+        </h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'user'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
